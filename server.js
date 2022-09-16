080592fa3fd9bfeb58f7f8a61711f165117a6b09
Jack Atkerson <jatkerson18@gmail.com>
2022-09-15 19:35:11 -0500

    Created Product model and integrated mongoose into server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,13 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGDB_URI || 'mongodb://localhost:27017/clothing-store', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
 
 app.listen(PORT, () => {
     console.log(`Server now connected on port ${PORT}`);
