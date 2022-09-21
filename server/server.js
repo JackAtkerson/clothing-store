d3db9c07a9c56cdc3ac17c3b15c49302acf1c5d8
Jack Atkerson <jatkerson18@gmail.com>
2022-09-21 16:57:09 -0500

    Fixed error in connection.js

--- server/server.js
+++ server/server.js
@@ -35,4 +35,13 @@ const PORT = process.env.PORT || 3001;
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-}
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+})
+};
+
+startApolloServer(typeDefs, resolvers);
